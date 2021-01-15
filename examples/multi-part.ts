9591660bb95e39e44fa4a47082e9c9a1cc87fea1
Matthias Jaros <jaros@ubilabs.net>
2021-01-15 11:56:37 +0100

    chore: update readme and examples (#10)

--- examples/multi-part.ts
+++ examples/multi-part.ts
@@ -11,4 +11,4 @@ const part2 = '!AIVDM,2,2,7,A,PBp888888888880,2*79';
 aisDecoder.write(part1);
 aisDecoder.write(part2);
 
-// {"type":5,"repeat":0,"mmsi":525200930,"aisVersion":2,"imo":9835915,"callsign":"YCKT2","name":"AKRA 102","typeAndCargo":80,"dimBow":67,"dimStern":33,"dimPort":19,"dimStarboard":1,"epfd":1,"etaMonth":9,"etaDay":7,"etaHour":16,"etaMinute":0,"draught":4.2,"destination":"ID_MERAK","dte":false}
+// {"type":5,"channel":"A","repeat":0,"mmsi":525200930,"aisVersion":2,"imo":9835915,"callsign":"YCKT2","name":"AKRA 102","typeAndCargo":80,"dimBow":67,"dimStern":33,"dimPort":19,"dimStarboard":1,"epfd":1,"etaMonth":9,"etaDay":7,"etaHour":16,"etaMinute":0,"draught":4.2,"destination":"ID_MERAK","dte":false,"sentences":["!AIVDM,2,1,7,A,57lof8`2F5HeT<eC:204e86373:222222222221@8HQC16Ch0:RA7kAD,0*28","!AIVDM,2,2,7,A,PBp888888888880,2*79"]}
