8e2a55b26aaee0a5c5193f268263497f62488c6d
Matthias Jaros <jaros@ubilabs.net>
2021-01-15 10:55:04 +0100

    revert readline-stream example

--- examples/readline-stream.ts
+++ examples/readline-stream.ts
@@ -5,12 +5,7 @@ import {createInterface} from 'readline';
 
 const aisDecoder = new AisDecoder();
 aisDecoder.on('error', err => console.error(err));
-aisDecoder.on('data', decodedMessage => {
-  const message = JSON.parse(decodedMessage);
-  if (message.type === 8) {
-    console.log(decodedMessage);
-  }
-});
+aisDecoder.on('data', decodedMessage => console.log(decodedMessage));
 
 const filePath = resolve(__dirname, './messages.txt');
 const fileStream = createReadStream(filePath);
